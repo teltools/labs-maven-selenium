9e292b921027c6f8d54eedeb1d3d96564f123a19
Leandro Humberto Vieira <leandrohmvieira@gmail.com>
2013-01-21 15:00:11 -0200

    Primeiro teste feito no Selenium e exportado para o Junit4, o teste consiste em descobrir se o elemento <h2>contatos</h2> existe na página http://www.teltools.com.br/index.php?op=contato

---
+++ teltools/src/main/java/com/teste/teltools/teltools/App.java
@@ -0,0 +1,13 @@
+package com.teste.teltools.teltools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ teltools/src/test/java/com/teste/teltools/teltools/testejunit.java
@@ -0,0 +1,65 @@
+package com.teste.teltools.teltools;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class testejunit {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://www.teltools.com.br/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testEjunit() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.linkText("Home")).click();
+    driver.findElement(By.linkText("Contato")).click();
+    try {
+      assertEquals("Driver", driver.findElement(By.cssSelector("h2.title")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alert.getText();
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
